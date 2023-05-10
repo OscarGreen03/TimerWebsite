4e92705d7753fd57eb1242f2ba696a2cb6c32f53
OscarGreen03 <greenorh@icloud.com>
2023-05-10 16:57:19 +0100

    Got it working but in a very rough state, need to add action and data information.

--- index.php
+++ index.php
@@ -101,8 +101,8 @@ fclose($fp);
         const data = encoder.encode(input);
         const hashBuffer = await crypto.subtle.digest('SHA-256', data);
         const hashArray = Array.from(new Uint8Array(hashBuffer));
-        const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-        return hashHex;
+        return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+        //return hashHex;
     }
 
 
